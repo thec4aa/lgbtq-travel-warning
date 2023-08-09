102406c149d6c4151ac6f9246717f4f39cc50fb8
Steve Lambert <67271+slambert@users.noreply.github.com>
2023-08-08 22:57:23 -0400

    fix: adjusting blur
    I want the sites to be more identifiable in screenshots. 🙂

--- content.js
+++ content.js
@@ -266,7 +266,7 @@ const bannerHTML = `
   overflow: auto; /* Enable scroll if needed */
   background-color: rgb(0, 0, 0); /* Fallback color */
   background-color: rgba(0, 0, 0, 0.4); /* Black w/ opacity */
-  backdrop-filter: blur(3px);
+  backdrop-filter: blur(2px);
 }
 
 /* Modal Content/Box */
